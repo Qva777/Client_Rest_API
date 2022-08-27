6b29b6d41673c898d5a082dbca414a2fbb6d2959
Qva777 <barshay.sasha@gmail.com>
2022-08-27 13:50:27 +0300

    add .env hidden SECRET_KEY

--- Client_Rest_config/SECRET_KEY.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = 'django-insecure-(qzpd+u6*p=mr+t^bztr(g^yxoqu_o@q(407)u-zo#x5dq&xlp'
--- Client_Rest_config/settings.py
+++ Client_Rest_config/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
-# from .SECRET_KEY import SECRET_KEY
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', "123")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "True")
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'client_API',
     'client_frontend',
+    'rest_framework_swagger',
     'djoser',
 ]
 
