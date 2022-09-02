20cb40b59d6496b095fd4debd20744eccd75720b
Qva777 <barshay.sasha@gmail.com>
2022-09-02 23:11:34 +0300

    Url fix viewset

--- client_API/urls.py
+++ client_API/urls.py
@@ -1,37 +1,27 @@
 from django.urls import path
-
 from client_API import views
-from .views import *
-
-# viewset
-# from client_API.views import ManagersViewSet
-# from rest_framework.routers import DefaultRouter
-#
-# router = DefaultRouter()
-# router.register(r'users', ManagersViewSet, basename='manager')
-# urlpatterns = router.urls
 
 app_name = 'task'
 urlpatterns = ([
+    # Create Task
     # path('create-task/', TaskCreateView.as_view(), name='create_task'),
 
+    # All Item List
     # path('all-tasks/', TaskListView.as_view(), name='all_tasks'),
     # path('all-managers/', ManagerListView.as_view(), name='all_managers'),
 
+    # Detail
     # path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
-
     # path('manager/<int:pk>/', ManagerDetailView.as_view(), name='manager_detail'),
-    # path('manager/put/<int:pk>/', ManagerDetailViewUpdate.as_view(), name='manager_detail_update'),
-
 
     # viewsSets
-    # path('all-managers/', views.ManagersViewSet.as_view({"get": "list"})),
-    # path('manager/<int:pk>/', views.ManagersViewSet.as_view(
-    #     {"get":"retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
-    #
-    # path('create-task/', views.TaskViewSet.as_view({"post": "create"})),
-    # path('all-tasks/', views.TaskViewSet.as_view({"get": "list"})),
-    # path('task/<int:pk>/', views.TaskViewSet.as_view(
-    #     {"get":"retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
+    path('all-managers/', views.ManagersViewSet.as_view({"get": "list"})),
+    path('manager/<int:pk>/', views.ManagersViewSet.as_view(
+        {"get":"retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
+
+    path('create-task/', views.TaskViewSet.as_view({"post": "create"})),
+    path('all-tasks/', views.TaskViewSet.as_view({"get": "list"})),
+    path('task/<int:pk>/', views.TaskViewSet.as_view(
+        {"get":"retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
 
 ])
