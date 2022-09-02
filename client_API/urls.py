318695e13e3f11cb76203c5527c24a474bd0396a
Qva777 <barshay.sasha@gmail.com>
2022-09-02 23:51:12 +0300

    PEP8 url and ViewSet

--- client_API/urls.py
+++ client_API/urls.py
@@ -2,26 +2,15 @@ from django.urls import path
 from client_API import views
 
 app_name = 'task'
+http_request = {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
 urlpatterns = ([
-    # Create Task
-    # path('create-task/', TaskCreateView.as_view(), name='create_task'),
-
-    # All Item List
-    # path('all-tasks/', TaskListView.as_view(), name='all_tasks'),
-    # path('all-managers/', ManagerListView.as_view(), name='all_managers'),
-
-    # Detail
-    # path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
-    # path('manager/<int:pk>/', ManagerDetailView.as_view(), name='manager_detail'),
-
     # viewsSets
-    path('all-managers/', views.ManagersViewSet.as_view({"get": "list"})),
-    path('manager/<int:pk>/', views.ManagersViewSet.as_view(
-        {"get":"retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
-
     path('create-task/', views.TaskViewSet.as_view({"post": "create"})),
+
+    path('task/<int:pk>/', views.TaskViewSet.as_view(http_request)),
+    path('manager/<int:pk>/', views.ManagersViewSet.as_view(http_request)),
+
     path('all-tasks/', views.TaskViewSet.as_view({"get": "list"})),
-    path('task/<int:pk>/', views.TaskViewSet.as_view(
-        {"get":"retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
+    path('all-managers/', views.ManagersViewSet.as_view({"get": "list"})),
 
 ])
