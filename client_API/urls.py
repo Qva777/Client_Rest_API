a15fd6abdab5acc2886a63c161ea303c4dd9e0ac
Qva777 <barshay.sasha@gmail.com>
2022-09-02 22:57:10 +0300

    add viewset

--- client_API/urls.py
+++ client_API/urls.py
@@ -13,21 +13,25 @@ from .views import *
 
 app_name = 'task'
 urlpatterns = ([
-    path('create-task/', TaskCreateView.as_view(), name='create_task'),
+    # path('create-task/', TaskCreateView.as_view(), name='create_task'),
 
-    path('all-tasks/', TaskListView.as_view(), name='all_tasks'),
-    path('all-managers/', ManagerListView.as_view(), name='all_managers'),
+    # path('all-tasks/', TaskListView.as_view(), name='all_tasks'),
+    # path('all-managers/', ManagerListView.as_view(), name='all_managers'),
 
-    path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    # path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
 
-    path('manager/<int:pk>/', ManagerDetailView.as_view(), name='manager_detail'),
+    # path('manager/<int:pk>/', ManagerDetailView.as_view(), name='manager_detail'),
     # path('manager/put/<int:pk>/', ManagerDetailViewUpdate.as_view(), name='manager_detail_update'),
 
 
     # viewsSets
-    # path('all-managers/', views.ManagersViewSet.as_view({"get": "list"})),
-    # path('manager/<int:pk>/', views.ManagersViewSet.as_view(
-    #     {"put": "update"}))#, "head": "", "options": ""})),
-    # path('manager/put/<int:pk>/', views.ManagersViewSet.as_view({"put": "retrieve"})),
+    path('all-managers/', views.ManagersViewSet.as_view({"get": "list"})),
+    path('manager/<int:pk>/', views.ManagersViewSet.as_view(
+        {"get":"retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
+
+    path('create-task/', views.TaskViewSet.as_view({"post": "create"})),
+    path('all-tasks/', views.TaskViewSet.as_view({"get": "list"})),
+    path('task/<int:pk>/', views.TaskViewSet.as_view(
+        {"get":"retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
 
 ])
--- client_API/views.py
+++ client_API/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 
 # from . import serializers
 # from .models import Manager, Task
+from rest_framework.viewsets import ModelViewSet
 
 from .permissions import IsOwnerOrReadOnly
 from client_API.serializers import *
@@ -15,35 +16,35 @@ from rest_framework.response import Response
 
 
 # class Task
-class TaskCreateView(generics.CreateAPIView):
-    """Создание задачи"""
-    serializer_class = TaskCreateSerializers
-    # permission_classes = (IsAuthenticated,)
-
-
-class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Информация про определенную задачу"""
-    serializer_class = TaskDetailSerializers
-    queryset = Task.objects.all()
-    # queryset = ManagerTask.objects.all()
-
-    permission_classes = (IsAuthenticated,)
-
-
-class TaskListView(generics.ListAPIView):
-    """Информация про все задачи"""
-    serializer_class = TaskListSerializers
-    queryset = Task.objects.all()
-    permission_classes = (IsOwnerOrReadOnly,)
+# class TaskCreateView(generics.CreateAPIView):
+#     """Создание задачи"""
+#     serializer_class = TaskCreateSerializers
+#     # permission_classes = (IsAuthenticated,)
+#
+#
+# class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """Информация про определенную задачу"""
+#     serializer_class = TaskDetailSerializers
+#     queryset = Task.objects.all()
+#     # queryset = ManagerTask.objects.all()
+#
+#     permission_classes = (IsAuthenticated,)
+#
+#
+# class TaskListView(generics.ListAPIView):
+#     """Информация про все задачи"""
+#     serializer_class = TaskListSerializers
+#     queryset = Task.objects.all()
+#     permission_classes = (IsOwnerOrReadOnly,)
 
 
 # Class Managers
-class ManagerDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Информация про определенную задачу"""
-    queryset = Manager.objects.all()
-    serializer_class = ManagerDetailSerializers
-    permission_classes = (IsAuthenticated,)
-#
+# class ManagerDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     """Информация про определенную задачу"""
+#     queryset = Manager.objects.all()
+#     serializer_class = ManagerDetailSerializers
+#     permission_classes = (IsAuthenticated,)
+# #
 #
 # class ManagerDetailViewUpdate(generics.RetrieveUpdateDestroyAPIView):
 #     """Обновление определенной задачи"""
@@ -52,11 +53,11 @@ class ManagerDetailView(generics.RetrieveUpdateDestroyAPIView):
 #     permission_classes = (IsAuthenticated,)
 #
 #
-class ManagerListView(generics.ListAPIView):
-    """Информация про все задачи"""
-    serializer_class = ManagerListSerializers
-    queryset = Manager.objects.all()
-    permission_classes = (IsOwnerOrReadOnly,)
+# class ManagerListView(generics.ListAPIView):
+#     """Информация про все задачи"""
+#     serializer_class = ManagerListSerializers
+#     queryset = Manager.objects.all()
+#     permission_classes = (IsOwnerOrReadOnly,)
 
 # VIEWSETS
 # class ManagersViewSet(viewsets.ReadOnlyModelViewSet):
@@ -82,12 +83,59 @@ class ManagerListView(generics.ListAPIView):
 #     A simple ViewSet for listing or retrieving users.
 #     """
 #
-#     queryset = Manager.objects.all()
-#     serializer = ManagerListSerializers(queryset, many=True)
+#     def list(self, request, *args, **kwargs):
+#         queryset = Manager.objects.all()
+#         serializer_class = ManagerListSerializers
+#         # serializer = ManagerListSerializers(queryset, many=True)
+#         return Response(serializer_class.data)
 #
+#     def update(self, request, pk=None, *args, **kwargs):
+#         queryset = Manager.objects.all()
 #         user = get_object_or_404(queryset, pk=pk)
 #         serializer = ManagerDetailSerializers(user)
+#         return Response(serializer.data)
 
+
+class ManagersViewSet(ModelViewSet):
+    # permission_classes = (IsAuthenticated,IsOwnerOrReadOnly)
+    def get_queryset(self):
+        return Manager.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return ManagerListSerializers
+        return ManagerDetailSerializers
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list':
+            permission_classes = [IsOwnerOrReadOnly]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+
+class TaskViewSet(ModelViewSet):
+
+    def get_queryset(self):
+        return Task.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return TaskListSerializers
+        return TaskDetailSerializers
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list' or self.action == 'create':
+            permission_classes = [IsOwnerOrReadOnly]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
 
 # class UserViewSet(viewsets.ViewSet):
 #     """
