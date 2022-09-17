39876811f15c0598545701810327f36a984bc1c5
Qva777 <barshay.sasha@gmail.com>
2022-09-17 15:01:45 +0300

    Moving Permissions

--- client_API/permissions.py
+++ client_API/permissions.py
@@ -1,4 +1,6 @@
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -18,3 +20,17 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.user == request.user
+
+
+class PermissionsViewSet(ModelViewSet):
+    def get_serializer_class(self):
+        """Возвращает класс, который должен быть использован для сериализатора."""
+        return self.serializer_classes.get(self.action)
+
+    def get_permissions(self):
+        """Создает экземпляр и возвращает список разрешений, которые требуются этому представлению."""
+        if self.action == 'list' or self.action == 'retrieve' or self.action == 'create':
+            permission_classes = [IsOwnerOrReadOnly]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
--- client_API/views.py
+++ client_API/views.py
@@ -1,23 +1,5 @@
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
-
-from .permissions import IsOwnerOrReadOnly
+from .permissions import PermissionsViewSet
 from client_API.serializers import *
-
-
-class PermissionsViewSet(ModelViewSet):
-    def get_serializer_class(self):
-        """Возвращает класс, который должен быть использован для сериализатора."""
-        return self.serializer_classes.get(self.action)
-
-    def get_permissions(self):
-        """Создает экземпляр и возвращает список разрешений, которые требуются этому представлению."""
-        # if self.action == 'list':
-        if self.action == 'list' or self.action == 'retrieve' or self.action == 'create':
-            permission_classes = [IsOwnerOrReadOnly]
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
 
 
 class ManagersViewSet(PermissionsViewSet):
