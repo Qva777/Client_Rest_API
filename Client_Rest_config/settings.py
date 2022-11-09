f9905ce3b8f394cec844cafd26f02405094009d3
Qva777 <barshay.sasha@gmail.com>
2022-11-09 22:15:32 +0200

    ALL ALLOWED_HOSTS

--- Client_Rest_config/settings.py
+++ Client_Rest_config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "123")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', "True")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
