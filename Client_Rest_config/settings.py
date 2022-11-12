fd2f4e9f85f51733bcfdcfb927a29f52a938ef4a
Qva777 <barshay.sasha@gmail.com>
2022-11-12 23:35:15 +0200

    PostgreSQl migrations Delete sqlite

--- Client_Rest_config/settings.py
+++ Client_Rest_config/settings.py
@@ -99,22 +99,13 @@ WSGI_APPLICATION = 'Client_Rest_config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# Sqlite
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
+# PostgreSQL
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbtest',
+        'NAME': 'db_client',
         'USER': 'postgres',
-        'PASSWORD': 'lolpop88',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
