29c9b2e5277abaea64bd594d76cd6b3caf24d0b1
Qva777 <barshay.sasha@gmail.com>
2022-11-17 22:28:10 +0200

    .env secret_key

--- first_start.bat
+++ first_start.bat
@@ -1,5 +1,5 @@
 python -m venv venv
 cd venv
 cd Scripts
-activate && cd ../.. && pip install -r requirements.txt  && python manage.py makemigrations ^
-&& python manage.py migrate && python manage.py runserver
+activate & cd ../.. & pip install -r requirements.txt  & echo  SECRET_KEY=YOUR_SECRET_KEY > .env & ^
+python manage.py makemigrations & python manage.py migrate & python manage.py runserver
