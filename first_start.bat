bb1a43c96c56aff1a47c2cef6fa0181a79548c3e
Qva777 <barshay.sasha@gmail.com>
2022-11-18 23:40:53 +0200

    First start create key

--- first_start.bat
+++ first_start.bat
@@ -1,5 +1,7 @@
 python -m venv venv
 cd venv
 cd Scripts
-activate & cd ../.. & pip install -r requirements.txt  & echo  SECRET_KEY=YOUR_SECRET_KEY > .env & ^
+activate & cd ../.. & pip install -r requirements.txt  & echo SECRET_KEY=YOUR_SECRET_KEY ^
+
+PASSWORD=123456 > .env & ^
 python manage.py makemigrations & python manage.py migrate & python manage.py runserver
