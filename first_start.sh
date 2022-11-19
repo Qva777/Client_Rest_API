335878834619452a24a3f3376dd392d6eb385bb4
Qva777 <barshay.sasha@gmail.com>
2022-11-19 23:09:49 +0200

    Rewrite password

--- first_start.sh
+++ first_start.sh
@@ -3,5 +3,5 @@ cd venv
 cd Scripts
 activate & cd ../.. & pip install -r requirements.txt  & echo SECRET_KEY=YOUR_SECRET_KEY ^
 
-PASSWORD=123456 > .env & ^
+PASSWORD=YOUR_PASSWORD_FOR_PgDB > .env & ^
 python manage.py makemigrations & python manage.py migrate & python manage.py runserver
