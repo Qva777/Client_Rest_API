572ea43497c0fdbb8260d43dd92b9885bc66bef7
Qva777 <barshay.sasha@gmail.com>
2022-11-21 22:49:03 +0200

    Activate venv in one str

--- first_start.sh
+++ first_start.sh
@@ -1,7 +1,5 @@
 python -m venv venv
-cd venv
-cd Scripts
-activate & cd ../.. & pip install -r requirements.txt  & echo SECRET_KEY=YOUR_SECRET_KEY ^
+.\venv\Scripts\activate & cd ../.. & pip install -r requirements.txt  & echo SECRET_KEY=YOUR_SECRET_KEY ^
 
 PASSWORD=YOUR_PASSWORD_FOR_PgDB > .env & ^
 python manage.py makemigrations & python manage.py migrate & python manage.py runserver
