6c4eb06b01c0b53b79ca83099bcc50918cdcea9d
Qva777 <barshay.sasha@gmail.com>
2022-11-22 18:54:39 +0200

    Delete cd up

--- first_start.sh
+++ first_start.sh
@@ -1,5 +1,5 @@
 python -m venv venv
-.\venv\Scripts\activate & cd ../.. & pip install -r requirements.txt  & echo SECRET_KEY=YOUR_SECRET_KEY ^
+.\venv\Scripts\activate &  pip install -r requirements.txt  & echo SECRET_KEY=YOUR_SECRET_KEY ^
 
 PASSWORD=YOUR_PASSWORD_FOR_PgDB > .env & ^
 python manage.py makemigrations & python manage.py migrate & python manage.py runserver
