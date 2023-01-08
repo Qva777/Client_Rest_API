aaa90573b6a354379ca48c41128c15b420dbcb8a
Qva777 <99911044+Qva777@users.noreply.github.com>
2023-01-08 18:46:43 +0200

    Update first_start.sh

--- first_start.sh
+++ first_start.sh
@@ -1,5 +1,9 @@
-python -m venv venv
-.\venv\Scripts\activate &  pip install -r requirements.txt  & echo SECRET_KEY=YOUR_SECRET_KEY ^
-
-PASSWORD=YOUR_PASSWORD_FOR_PgDB > .env & ^
-python manage.py makemigrations & python manage.py migrate & python manage.py runserver
+#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+echo SECRET_KEY=YOUR_SECRET_KEY > .env
+echo DEBUG=True > .env
+PASSWORD=YOUR_PASSWORD_FOR_PgDB > .env
+python3 manage.py migrate
+python3 manage.py runserver
