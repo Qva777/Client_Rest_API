0170b7f46e85fb6013e45ffc37ba25f8be95361c
Qva777 <99911044+Qva777@users.noreply.github.com>
2023-01-12 13:50:50 +0200

    Update first_start.bat

--- first_start.bat
+++ first_start.bat
@@ -1,6 +1,8 @@
 python -m venv venv
-.\venv\Scripts\activate & pip install -r requirements.txt
+.\venv\Scripts\activate 
+pip install -r requirements.txt
 echo SECRET_KEY=YOUR_SECRET_KEY > .env
 echo DEBUG=True >> .env
-PASSWORD=YOUR_PASSWORD_FOR_PgDB >> .env & ^
-python manage.py makemigrations & python manage.py migrate & python manage.py runserver
+PASSWORD=YOUR_PASSWORD_FOR_PgDB >> .env
+python manage.py migrate
+python manage.py runserver
